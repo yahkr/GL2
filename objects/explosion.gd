c8088a47cdf170bfd829d718a3be952dd380a75c
Björn Feber <bfeber@protonmail.com>
2023-01-21 11:58:05 +0100

    Fix explosion damage

--- objects/explosion.gd
+++ objects/explosion.gd
@@ -14,5 +14,6 @@ func _physics_process(delta: float) -> void:
 		if player:
 			var dist := shapecast.get_collision_point(index).distance_squared_to(global_position)
 			player.health -= int(exp((36 - dist) * 0.14))
+	shapecast.collide_with_bodies = false
 	await get_tree().create_timer(lifetime).timeout
 	queue_free()
